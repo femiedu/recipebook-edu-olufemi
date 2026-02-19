0b56fbf06fec68c8b9603fcb2da3c533626b1d1a
Femi Edu <olufemi.edu@student.ateneo.edu>
2026-02-19 22:14:31 +0800

    create models and make migrations

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ledger:recipe-detail", args=[self.pk])
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="ingredients",
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="recipe",
+    )
+    quantity = models.CharField(max_length=50)
