86a2ef566b44cf2576cf22aa32ca28d3c3364678
Femi Edu <olufemi.edu@student.ateneo.edu>
2026-02-19 22:44:29 +0800

    change views to support database

--- ledger/views.py
+++ ledger/views.py
@@ -1,55 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
-RECIPES_LIST = [
-    {"name": "Recipe 1", "link": "/recipe/1"},
-    {"name": "Recipe 2", "link": "/recipe/2"},
-]
-
-RECIPE_1_DATA = {
-    "name": "Recipe 1",
-    "ingredients": [
-        {"name": "tomato", "quantity": "3pcs"},
-        {"name": "onion", "quantity": "1pc"},
-        {"name": "pork", "quantity": "1kg"},
-        {"name": "water", "quantity": "1L"},
-        {"name": "sinigang mix", "quantity": "1 packet"},
-    ],
-}
-
-RECIPE_2_DATA = {
-    "name": "Recipe 2",
-    "ingredients": [
-        {"name": "garlic", "quantity": "1 head"},
-        {"name": "onion", "quantity": "1pc"},
-        {"name": "vinegar", "quantity": "1/2cup"},
-        {"name": "water", "quantity": "1 cup"},
-        {"name": "salt", "quantity": "1 tablespoon"},
-        {"name": "whole black peppers", "quantity": "1 tablespoon"},
-        {"name": "pork", "quantity": "1 kilo"},
-    ],
-}
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
 
-def recipes_list(request):
-    return render(
-        request,
-        "ledger/recipes_list.html",
-        {"recipes": RECIPES_LIST},
-    )
+def recipe_list(request):
+    recipes = Recipe.objects.all().order_by("name")
+    return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 
 
-def recipe_1(request):
-    return render(
-        request,
-        "ledger/recipe_detail.html",
-        {"recipe": RECIPE_1_DATA},
-    )
-
-
-def recipe_2(request):
-    return render(
-        request,
-        "ledger/recipe_detail.html",
-        {"recipe": RECIPE_2_DATA},
-    )
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
