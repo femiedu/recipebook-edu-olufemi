12a7baba9abb96fc78b64f222b66f2d50b93c106
Femi Edu <olufemi.edu@student.ateneo.edu>
2026-02-19 22:48:45 +0800

    change urls

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "ledger"
+
 urlpatterns = [
-    path("recipes/list", views.recipes_list, name="recipes-list"),
-    path("recipe/1", views.recipe_1, name="recipe-1"),
-    path("recipe/2", views.recipe_2, name="recipe-2"),
+    path("", views.recipe_list, name="recipe-list"),
+    path("recipe/<int:pk>/", views.recipe_detail, name="recipe-detail"),
 ]
