3a461c5e181e312ade282bd42ef630540a3332df
Femi Edu <olufemi.edu@student.ateneo.edu>
2026-03-12 14:22:34 +0800

    added new urls recipe_add and recipe_addimage

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,5 +5,7 @@ app_name = "ledger"
 
 urlpatterns = [
     path("recipes/list/", views.recipe_list, name="recipe-list"),
+    path("recipes/add/", views.recipe_add, name="recipe-add"),
     path("recipes/<int:pk>/", views.recipe_detail, name="recipe-detail"),
+    path("recipes/<int:pk>/add_image/", views.recipe_image_add, name="recipe-image-add"),
 ]
